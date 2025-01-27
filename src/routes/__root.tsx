5199f0bba8286ce8d140716b868c90039032947c
David Basile Filho <davidbasilefilho@gmail.com>
2025-01-27 12:22:12 -0300

    disabled router dev tools in prod

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,11 +1,24 @@
 import { createRootRoute, Outlet } from "@tanstack/react-router";
-import { TanStackRouterDevtools } from "@tanstack/router-devtools";
+import React, { Suspense } from "react";
+
+const TanStackRouterDevtools =
+  process.env.NODE_ENV === "production"
+    ? () => null
+    : React.lazy(() =>
+        import("@tanstack/router-devtools").then((res) => ({
+          default: res.TanStackRouterDevtools,
+        }))
+      );
 
 export const Route = createRootRoute({
   component: () => (
     <div className="background-gradient">
       <Outlet />
-      <TanStackRouterDevtools />
+      {process.env.NODE_ENV !== "production" && (
+        <Suspense fallback={null}>
+          <TanStackRouterDevtools />
+        </Suspense>
+      )}
     </div>
   ),
 });
